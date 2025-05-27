c0ae5a9f5378618bda6be134399210316d806c41
Akpan victor <vakpan462@example.com>
2025-05-27 20:16:38 +0100

    first commit

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './home.css';
-import profilePic from './assets/pfp.jpg'; // adjust path if needed
+import pfp from '../../assets/pfp.jpg'; // Adjust the path if necessary
 
 const Home = () => {
   return (
     <section className="home-anime-container" id="home">
       <div className="home-anime-overlay" />
       <div className="home-anime-content">
-        <img src={profilePic} alt="Victor" className="anime-avatar" />
+        <img src={pfp} alt="Victor" className="anime-avatar" />
         <div className="anime-text">
           <h1 className="anime-name">Hi, My name is Akpan Victor</h1>
-          <h2 className="anime-title">Full Stack Software Developer</h2>
+          <h2 className="anime-title">Full Stack Software Developer </h2>
           <p className="anime-quote">“I am a passionate individual.”</p>
         </div>
       </div>
