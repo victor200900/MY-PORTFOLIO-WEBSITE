9f310f6cfd6af0cd2446f6ba98b71cfcf991f21a
Akpan victor <vakpan462@example.com>
2025-06-03 15:20:24 +0100

    first commit

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -15,7 +15,7 @@ const About = () => {
         </div>
 
         <div className="about-right" data-aos="fade-left">
-          <h2 className="about-title" data-aos="fade-down">About Me</h2>
+          <h2 className="about-title" data-aos="fade-left">About Me</h2>
           <p className="about-text" data-aos="fade-up" data-aos-delay="200">
             I’m <strong>Victor</strong>, a creative frontend developer and passionate anime fan. I blend clean UI design with responsive functionality — turning code into art.  
             <br /> <br />
